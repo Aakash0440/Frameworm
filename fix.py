bdb1ec0a887f01c58550ee4e3870133d3306527d
Aakash0440 <Aakashali.03002985610@gmail.com>
2026-03-03 18:16:36 +0500

    chore: remove stale fix scripts and fragmented test files

--- fix.py
+++
@@ -1,6 +0,0 @@
-with open('training/trainer.py') as f:
-    content = f.read()
-content = content.replace("'step': self.global_step,", "'step': self.state.global_step,")
-with open('training/trainer.py', 'w') as f:
-    f.write(content)
-print('Fixed')
